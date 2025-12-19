4e8e6ade2c05ce1459da1efcfb5496624b486161
Roaa Arafat <roaamaher69@gmail.com>
2025-12-19 22:11:26 +0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,18 @@
+
+package w9;
+
+public class A{
+    
+    public void m1(){
+        System.out.println("m1 from A");
+    }
+    
+     public void m2(){
+        System.out.println("m2 from A");
+    }
+     
+      public void m3(){
+        System.out.println("m3 from A");
+    }
+     
+    }
---
+++ B.java
@@ -0,0 +1,19 @@
+
+package w9;
+
+
+public class B extends A{
+    
+      public void m3(){
+        System.out.println("m3 from B");
+    }
+    
+      public void m4(){
+        System.out.println("m3 from B");
+    }
+      
+        public void m5(){
+        System.out.println("m3 from B");
+    }
+      
+}
